2f4cdb49685735cd17fe27cd1a2486277cb25c83
justin <justin@kali.mineduc.co.rw>
2025-01-27 21:41:39 +0200

    first commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose';
-import { config } from 'dotenv';
+import mongoose from "mongoose";
+import { config } from "dotenv";
 
 config(); // Load .env variables
 
@@ -7,16 +7,16 @@ const connectDB = async (): Promise<void> => {
   try {
     const uri = process.env.MONGO_URI as string;
     if (!uri) {
-      throw new Error('MongoDB URI is not defined in .env file');
+      throw new Error("MongoDB URI is not defined in .env file");
     }
 
     await mongoose.connect(uri);
-    console.log('✅ MongoDB Connected...');
+    console.log("✅ MongoDB Connected...");
   } catch (err) {
     if (err instanceof Error) {
-      console.error('❌ MongoDB Connection Failed:', err.message);
+      console.error("❌ MongoDB Connection Failed:", err.message);
     } else {
-      console.error('❌ MongoDB Connection Failed:', err);
+      console.error("❌ MongoDB Connection Failed:", err);
     }
     process.exit(1);
   }
--- src/config/passportConfig.ts
+++ src/config/passportConfig.ts
@@ -1,18 +1,23 @@
-const passport = require('passport');
-const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt');
-const User = require('../models/User');
+const passport = require("passport");
+const { ExtractJwt, Strategy: JwtStrategy } = require("passport-jwt");
+const User = require("../models/User");
 const { JWT_SECRET } = process.env;
 
-passport.use(new JwtStrategy({
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: JWT_SECRET
-}, async (payload: any, done: (error: any, user?: any) => void) => {
-    try {
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: JWT_SECRET,
+    },
+    async (payload: any, done: (error: any, user?: any) => void) => {
+      try {
         const user = await User.findById(payload.userId);
         if (!user) {
-            return done(null, false);
+          return done(null, false);
         }
-    } catch (error) {
+      } catch (error) {
         done(error, false);
+      }
     }
-}));
+  )
+);
